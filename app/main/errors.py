4669afcd63822862fb80fd7627298fe1aba5d971
Adrián Álvarez Sáez <adalsa91@gmail.com>
2018-08-10 00:40:31 +0200

    Añadido soporte bootstrap para flask y creada plantilla base.

--- app/main/errors.py
+++ app/main/errors.py
@@ -3,6 +3,7 @@ from . import main
 
 @main.app_errorhandler(404)
 def page_not_found(e):
+    pass
     return render_template('404.html'), 404
 
 @main.app_errorhandler(500)
--- app/main/views.py
+++ app/main/views.py
@@ -5,3 +5,8 @@ from . import main
 @main.route('/', methods=['GET', 'POST'])
 def hello_world():
     return render_template('index.html')
+
+@main.route('/base', methods=['GET', 'POST'])
+def base():
+    return render_template('base.html')
+
