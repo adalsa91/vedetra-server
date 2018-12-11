98599da4ba526bb305f021afaf7ce4128dc12813
Adrián Álvarez Sáez <adalsa91@gmail.com>
2018-12-11 19:33:38 +0100

    Añadido soporte para migraciones de base de datos (Closes #4) y rediseño de la plantilla base.

--- app/main/views.py
+++ app/main/views.py
@@ -3,17 +3,21 @@ from flask import Flask, request, render_template
 from . import main
 from .. import db
 
+
 @main.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
+
 
 @main.route('/base', methods=['GET', 'POST'])
 def base():
     return render_template('base.html')
 
+
 @main.route('/detecciones', methods=['GET', 'POST'])
 def detecciones():
     return render_template('detecciones.html')
+
 
 @main.route('/trayectos', methods=['GET', 'POST'])
 def trayectos():
